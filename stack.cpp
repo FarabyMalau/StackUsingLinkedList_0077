ccfd231b5a2c7b32185dafb4e36ebe0d79567566
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-05 18:04:48 +0600

    case 1

--- stack.cpp
+++ stack.cpp
@@ -96,8 +96,11 @@ int main()
 
         switch (choice)
         {
-        
-            
+            case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); // push the entered value onto the stack
+            break;            
         }
     }
 }
